219c142fc9ab257e19ac49252335cf8db0e121f0
Roosham <bernice.huang101@gmail.com>
2025-08-02 11:16:47 -0400

    ANW P1

---
+++ Amazing Ninja Worlds - Part 1/Assets/Scripts/PickUpKey.cs
@@ -0,0 +1,31 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpKey : MonoBehaviour
+{
+    SpriteRenderer m_renderer;
+    public ParticleSystem m_particle;
+
+    public ParticleSystem allParticles;
+
+    public GameObject HealthBar;
+
+
+    private void Start()
+    {
+        m_renderer = GetComponentInParent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        m_renderer.enabled = false;
+        m_particle.Stop();
+        allParticles.Play();
+        GetComponent<LifeHUD>();
+        HealthBar.GetComponent<LifeHUD>().HealPlayer();
+        //Debug.Log("Hit!");
+    }
+
+}
